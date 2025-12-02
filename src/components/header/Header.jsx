118a7da58482a03e7e1d708630edd158e595c82e
NrgNinja <diazrenato2001@outlook.com>
2025-12-02 04:19:04 -0500

    updated header image with new headshot

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/renato-ai-nobg.png'
+import ME from '../../assets/renato-nobg.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
